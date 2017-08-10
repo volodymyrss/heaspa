9ae520cdf20f7a3e9ea068ebea9bb57f4fcce675
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2017-08-10 14:43:49 +0200

    test for pha

--- tests/test_rw.py
+++ tests/test_rw.py
@@ -1,4 +1,4 @@
-def test_write():
+def test_rmf_write():
 
     import heaspa
     import numpy as np
@@ -11,3 +11,16 @@ def test_write():
             np.array([[1,0],[0,1]]))
 
     rmf.write("test_rmf.fits")
+
+def test_pha_write():
+
+    import heaspa
+    import numpy as np
+
+    rmf=heaspa.PHA(
+            np.array([1,2]),
+            np.array([2,3]),
+            1.
+        )
+
+    rmf.write("test_pha.fits")
